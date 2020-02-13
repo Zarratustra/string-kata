253bb680903407663fb95ef14a5cee05e866280d
Michał Hamuda <hamud94@gmail.com>
2020-02-13 08:55:20 +0100

    One number

--- src/main/java/com/michal/kata/StringCalculator.java
+++ src/main/java/com/michal/kata/StringCalculator.java
@@ -2,8 +2,19 @@ package com.michal.kata;
 
 public class StringCalculator {
 
-    int add(String numbers){
-        return 0;
+    int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        } else {
+            try {
+                int value = Integer.parseInt(numbers);
+                return value;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Calculator input didn't contain numbers in proper format");
+            }
+
+        }
+
     }
 
 }
--- src/test/java/com/michal/kata/StringCalculatorTest.java
+++ src/test/java/com/michal/kata/StringCalculatorTest.java
@@ -1,7 +1,10 @@
 package com.michal.kata;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
 
@@ -10,6 +13,21 @@ public class StringCalculatorTest {
     @Test
     void emptyString() {
         assertEquals(0, calculator.add(""), "Wrong output for empty string input");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-45,0,23})
+    void oneNumber(int value) {
+        assertEquals(value, calculator.add(""+value), "Wrong output for input '"+value+"'");
+    }
+
+    @Test
+    void badFormat() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.add("ybfg"),
+                "Calculator didn't throw exception on malformed input"
+        );
     }
 
 

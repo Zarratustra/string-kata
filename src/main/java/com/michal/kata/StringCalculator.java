e01a4f7f661a2839e48d5fa0bba0fec547f580d5
Michał Hamuda <hamud94@gmail.com>
2020-02-13 10:17:10 +0100

    5. Reject negative numbers

--- src/main/java/com/michal/kata/StringCalculator.java
+++ src/main/java/com/michal/kata/StringCalculator.java
@@ -1,6 +1,8 @@
 package com.michal.kata;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
@@ -24,16 +26,43 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         } else {
-            try {
-                String[] separatedNumbers = numbers.split(separatorRegex, -1);
-                int sum = Arrays.stream(separatedNumbers)
-                        .mapToInt(Integer::parseInt)
-                        .sum();
-                return sum;
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Calculator input didn't contain numbers in proper format");
-            }
+            List<Integer> parsedNumbers = parseNumbers(numbers, separatorRegex);
+            validateNoNegativeNumbers(parsedNumbers);
+
+            return sumNumbers(parsedNumbers);
         }
+    }
+
+    private List<Integer> parseNumbers(String numbers, String separatorRegex) {
+        try {
+            String[] separatedNumbers = numbers.split(separatorRegex, -1);
+            return Arrays
+                    .stream(separatedNumbers)
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Calculator input didn't contain numbers in proper format");
+        }
+    }
+
+    private void validateNoNegativeNumbers(List<Integer> numbers) {
+        List<String> negativeNumbers = numbers
+                .stream()
+                .filter(number -> number < 0)
+                .map(Object::toString)
+                .collect(Collectors.toList());
+        if (!negativeNumbers.isEmpty()) {
+            String negativeNumbersString = String.join(",", negativeNumbers);
+            throw new IllegalArgumentException("Negative numbers not allowed: " + negativeNumbersString);
+        }
+    }
+
+    private int sumNumbers(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
 }
--- src/test/java/com/michal/kata/StringCalculatorTest.java
+++ src/test/java/com/michal/kata/StringCalculatorTest.java
@@ -16,34 +16,34 @@ public class StringCalculatorTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-45,0,23})
+    @ValueSource(ints = {0, 23, 275})
     void oneNumber(int value) {
-        calculatorAssertion(""+value,value);
+        calculatorAssertion("" + value, value);
     }
 
     @Test
     void twoNumbers() {
-        calculatorAssertion("2,3",5);
+        calculatorAssertion("2,3", 5);
     }
 
     @Test
     void multipleNumbers() {
-        calculatorAssertion("1,2,3,4,5",15);
+        calculatorAssertion("1,2,3,4,5", 15);
     }
 
     @Test
     void commasAndNewlines() {
-        calculatorAssertion("1\n2,3\n4",10);
+        calculatorAssertion("1\n2,3\n4", 10);
     }
 
     @Test
     void arbitraryDelimiter() {
-        calculatorAssertion("//;\n1;2;3;4",10);
+        calculatorAssertion("//;\n1;2;3;4", 10);
     }
 
     @Test
     void arbitraryDelimiterNoNumbers() {
-        calculatorAssertion("//;\n",0);
+        calculatorAssertion("//;\n", 0);
     }
 
     @Test
@@ -65,7 +65,33 @@ public class StringCalculatorTest {
     }
 
     void calculatorAssertion(String input, int expectedOutput) {
-        assertEquals(expectedOutput, calculator.add(input), "Wrong output for input '"+input+"'");
+        assertEquals(expectedOutput, calculator.add(input), "Wrong output for input '" + input + "'");
+    }
+
+    @Test
+    void rejectOneNegative() {
+        negativeNumbersTestCase("-1","-1");
+    }
+
+    @Test
+    void rejectMultipleNegatives() {
+        negativeNumbersTestCase("-1\n2,-3\n4","-1,-3");
+    }
+
+    private void negativeNumbersTestCase(String input, String negativeNumbers) {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.add(input),
+                "Calculator didn't throw exception on input with negative number(s)"
+        );
+
+        try {
+            calculator.add(input);
+        } catch (IllegalArgumentException e) {
+            String message = e.getMessage();
+            String expectedMessage = "Negative numbers not allowed: " + negativeNumbers;
+            assertEquals(message, expectedMessage);
+        }
     }
 
     @Test
